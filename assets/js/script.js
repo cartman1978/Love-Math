7fc055373fd66978f06a02e19ebd0de3731f4be9
Fabio Torti <cartman_7802@yahoo.ie>
2021-02-08 16:02:56 +0000

    added function for the division

--- assets/js/script.js
+++ assets/js/script.js
@@ -41,6 +41,8 @@ function runGame(gameType) {
     displayMultiplyQuestion(num1, num2);
   } else if(gameType === 'subtract') {
     dispalySubtractQuestion(num1, num2);
+  } else if(gameType === 'division') {
+    displayDividingQuestion(num1, num2);
   }
 }
 
@@ -80,6 +82,8 @@ function calculateCorrectAnswer() {
     return [operand1 * operand2, 'multiply'];
   } else if(operator === '-') {
     return [operand1 - operand2, 'subtract'];
+  } else if(operator === '/') {
+    return [operand1 / operand2, 'division'];
   }
 }
 
@@ -113,4 +117,10 @@ function displayMultiplyQuestion(operand1, operand2) {
     document.getElementById('operand1').textContent = operand1;
     document.getElementById('operand2').textContent = operand2;
     document.getElementById('operator').textContent = 'x';
+}
+
+function displayDividingQuestion(operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1 > operand2 ? Math.ceil(operand1) :Math.ceil(operand2);
+    document.getElementById('operand2').textContent = operand2 < operand1 ? operand2 : operand1;
+    document.getElementById('operator').textContent = '/';
 }

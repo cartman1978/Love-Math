1bbf52d263e32df2e5cb49b4150a7e654f94e8d0
Fabio Torti <cartman_7802@yahoo.ie>
2021-02-08 13:48:50 +0000

    added sutraction function to script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -29,7 +29,8 @@ function runGame(gameType) {
     displayAdditionQuestion(num1, num2);
   } else if (gameType === 'multiply'){
     displayMultiplyQuestion(num1, num2);
-   
+  } else if(gameType === 'subtract') {
+    dispalySubtractQuestion(num1, num2);
   }
 }
 
@@ -65,8 +66,10 @@ function calculateCorrectAnswer() {
 
   if(operator === '+') {
     return [operand1 + operand2, 'addition'];
-  } else if(operator === '*'){
+  } else if(operator === 'x'){
     return [operand1 * operand2, 'multiply'];
+  } else if(operator === '-') {
+    return [operand1 - operand2, 'subtract'];
   }
 }
 
@@ -87,12 +90,17 @@ function displayAdditionQuestion(operand1, operand2) {
     document.getElementById('operator').textContent = '+';
 }
 
-function dispalySubtractQuestion() {
+function dispalySubtractQuestion(operand1, operand2) {
+  // display opernad1 bigger thaf operand2
+  
+    document.getElementById('operand1').textContent = operand1 > operand2 ? operand1 : operand2;
+    document.getElementById('operand2').textContent = operand2 < operand1 ? operand2 : operand1;
+    document.getElementById('operator').textContent = '-';
 
 }
 
 function displayMultiplyQuestion(operand1, operand2) {
     document.getElementById('operand1').textContent = operand1;
     document.getElementById('operand2').textContent = operand2;
-    document.getElementById('operator').textContent = '*';
+    document.getElementById('operator').textContent = 'x';
 }
